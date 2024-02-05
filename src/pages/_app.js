355f75ed7f50e10a5c59d6dfd790869783ff5a11
FACUNDEZ <facu.mdq@live.com>
2024-02-05 17:54:12 -0300

    finished x2

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,11 @@
 import '@/styles/globals.css'
 import UserProvider from '@/context/UserContext'
-import ListUserProvider from '@/context/ListUsersContext'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <UserProvider>
-        <ListUserProvider>
         <Component {...pageProps} />
-        </ListUserProvider>
       </UserProvider>
     </>
   )

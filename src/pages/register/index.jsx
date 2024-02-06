1bd1b423ce77850a1669b96440477cd7e7d05168
FACUNDEZ <facu.mdq@live.com>
2024-02-06 17:04:46 -0300

    ..ñññ.

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -13,7 +13,7 @@ function Page() {
 
     const getApi = async () => {
         try {
-            const api = "https://localhost:3000/api/usuarios/register"
+            const api = "https://chatsim.netlify.app/api/usuarios/register"
             const response = await fetch(api, {
                 method: 'POST',
                 headers: {

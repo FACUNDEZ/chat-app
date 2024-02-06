1673f593dab1a644232b142a911d665b03d5cadf
FACUNDEZ <facu.mdq@live.com>
2024-02-06 16:33:39 -0300

    ready

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
   const getData = async () => {
     try {
-      const api = "http://localhost:3000/api/usuarios/login"
+      const api = "http://chatsim.netlify.app/api/usuarios/login"
       const response = await fetch(api, {
         method: 'POST',
         headers: {

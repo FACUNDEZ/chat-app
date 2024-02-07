2fb06d413b3e942eefd4f6d2a2b17fb86dcc1ee0
FACUNDEZ <facu.mdq@live.com>
2024-02-07 17:07:50 -0300

    as

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 
   const getData = async () => {
     try {
-      const api = "http://192.168.0.210:3000/api/usuarios/login"
+      const api = "https://192.168.0.210:3000/api/usuarios/login"
       const response = await fetch(api, {
         method: 'POST',
         headers: {
